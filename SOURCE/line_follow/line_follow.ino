100b40c614db641d6cd5d26d3a12a56c7f817984
Vido <vido@usp.br>
2014-12-20 20:22:57 -0200

    2nd is the 1st loser! Code for Garoa Kumite

--- SOURCE/line_follow/line_follow.ino
+++ SOURCE/line_follow/line_follow.ino
@@ -8,49 +8,107 @@
 void setup() {
   Serial.begin(9600);
   
+  pinMode(FAR_RIGHT, INPUT);  
   pinMode(RIGHT, INPUT);
   pinMode(CENTER_RIGHT, INPUT);
   pinMode(CENTER_LEFT, INPUT);
   pinMode(LEFT, INPUT);
+  pinMode(FAR_LEFT, INPUT);
   
   left_motor.attach(LEFT_MOTOR_PIN);
   right_motor.attach(RIGHT_MOTOR_PIN);
 }
 
+int state = 0; 
+
 void loop() {
-  
-    int seg1 = analogRead(RIGHT);
-    int seg2 = (analogRead(CENTER_RIGHT) + analogRead(CENTER_LEFT))/2;
-    int seg3 = analogRead(LEFT);
-  
+
+    boolean r0 = toDigital(FAR_LEFT);  
+    boolean r1 = toDigital(LEFT);
+    boolean r2 = toDigital(CENTER_LEFT);
+    boolean r3 = toDigital(CENTER_RIGHT);
+    boolean r4 = toDigital(RIGHT);
+    boolean r5 = toDigital(FAR_RIGHT);
+
     #ifdef LF_DEBUG
-      Serial.print(seg1);
+      Serial.print(r0);
       Serial.print("\t");
-      Serial.print(seg2);
+      Serial.print(r1);
       Serial.print("\t");
-      Serial.println(seg3);
+      Serial.print(r2);
+      Serial.print("\t");
+      Serial.print(r3);
+      Serial.print("\t");
+      Serial.print(r4);
+      Serial.print("\t");
+      Serial.println(r5);
+      Serial.println(state);
     #endif
 
-    if(seg1 > 700 && seg3 > 700){
-      frente();
-    }else{
-      if(seg1 > 700 && seg2 > 700){
-        esquerda_suave();
-      }else
-      if(seg3 > 700 && seg2 > 700){
-        direita_suave();
-      }else
-      if(seg1 > 700 && seg2 < 700){
-        esquerda();
-      }else
-      if(seg3 > 700 && seg2 < 700){
-        direita();
-      }else
-      if(seg2 > 700 && seg1 < 700 && seg3 < 700){
-        frente();
-      }else{
-        frente();
-      }
+    if(!r0 && r1 && r2 && r3 && r4 && r5){
+      state = -3;
     }
-  // delay(1);
+    else if(!r0 && !r1 && r2 && r3 && r4 && r5){
+      state = -2;
+    }
+    else if(r0 && !r1 && !r2 && r3 && r4 && r5){
+      state = -1;
+    }
+    else if(r0 && r1 && !r2 && !r3 && r4 && r5){
+      state = 0;
+    }
+    else if(r0 && r1 && r2 && !r3 && !r4 && r5){
+      state = 1;
+    }
+    else if(r0 && r1 && r2 && r3 && !r4 && !r5){
+      state = 2;
+    }
+    else if(r0 && r1 && r2 && r3 && r4 && !r5){
+      state = 3;
+    }
+    else if(r0 && r1 && r2 && r3 && r4 && r5){
+      // Tudo 1
+      state = state > 0 ? 4 : -4;
+    }else{
+      // fica loko
+    }
+    
+    switch(state){
+      case -4:
+          left_motor.write(PPM_MAX);
+          right_motor.write(PPM_MIN);
+          break;
+      case -3:
+          left_motor.write(PPM_SLOW);
+          right_motor.write(PPM_SLOW_BACK);
+          break;
+      case -2:
+          left_motor.write(PPM_SLOW);
+          right_motor.write(PPM_IDLE);
+          break;
+      case -1:
+          left_motor.write(PPM_MAX);
+          right_motor.write(PPM_SLOW);
+          break;
+      case 0:
+          left_motor.write(PPM_MAX);
+          right_motor.write(PPM_MAX);
+          break;
+      case 1:
+          left_motor.write(PPM_SLOW);
+          right_motor.write(PPM_MAX);
+          break;
+      case 2:
+          left_motor.write(PPM_SLOW_BACK);
+          right_motor.write(PPM_SLOW);
+          break;
+      case 3:
+          left_motor.write(PPM_SLOW_BACK);
+          right_motor.write(PPM_SLOW);
+          break;
+      case 4:
+          left_motor.write(PPM_MIN);
+          right_motor.write(PPM_MAX);
+          break;
+    }
 }
--- SOURCE/line_follow/scorpion_xl.h
+++ SOURCE/line_follow/scorpion_xl.h
@@ -1,10 +1,10 @@
 #include <Servo.h>
 
-#define PPM_MAX 125
+#define PPM_MAX 180-18
+#define PPM_SLOW 130
 #define PPM_IDLE 90
-#define PPM_SLOW 115
-#define PPM_REV 55
-#define PPM_MIN 40
+#define PPM_SLOW_BACK 50
+#define PPM_MIN 0+18
 
 #define LEFT_MOTOR_PIN 6
 #define RIGHT_MOTOR_PIN 10
@@ -13,28 +13,3 @@
 // Scorpion XL PPM Signal
 Servo left_motor;
 Servo right_motor;
-
-void frente() {
-  left_motor.write(PPM_MAX);
-  right_motor.write(PPM_MAX);
-}
-
-void direita() {
-  left_motor.write(PPM_MAX);
-  right_motor.write(PPM_REV);
-}
-
-void esquerda() {
-  left_motor.write(PPM_REV);
-  right_motor.write(PPM_MAX);
-}
-
-void direita_suave() {
-  left_motor.write(PPM_MAX);
-  right_motor.write(PPM_IDLE);
-}
-
-void esquerda_suave() {
-  left_motor.write(PPM_IDLE);
-  right_motor.write(PPM_MAX);
-}
